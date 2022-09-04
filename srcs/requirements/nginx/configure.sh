54fd6c99f0baf2b923859c6a1c16f070091abfbb
jkong <jkong@student.42seoul.kr>
2022-09-05 03:00:54 +0900

    Configure nginx and make WordPress healthy

---
+++ srcs/requirements/nginx/configure.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+mkdir -p /var/nginx/errors/
+cat > /var/nginx/errors/404.html << 'EOF'
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>The page you were looking for doesn't exist (404)</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<style>
+
+	body {
+		background-color: #2A292F;
+		color: #00BBBD;
+		text-align: center;
+		font-family: arial, sans-serif;
+		margin: 0;
+	}
+
+	h1, h2 {
+		font-weight: normal;
+		margin: 50px auto;
+	}
+	</style>
+</head>
+
+<body>
+	<h1>Seems like your page doesn't exist anymore !</h1>
+</body></html>
+EOF
+
+cat > /etc/nginx/http.d/default.conf << EOF
+server {
+	ssl_certificate sslcert/certificate.pem;
+	ssl_certificate_key sslcert/key.pem;
+
+	listen 443 default_server ssl;
+	listen [::]:443 default_server ssl;
+
+	server_name $DOMAIN_NAME;
+
+	root /var/www;
+
+	error_page 404 /errors/404.html;
+	access_log logs/access.log main;
+
+	index index.php index.html index.htm;
+
+	location ~ ^/(errors)/ {
+		root /var/nginx;
+		expires 30d;
+	}
+
+	# static file 404's aren't logged and expires header is set to maximum age
+	location ~* \.(jpg|jpeg|gif|css|png|js|ico|html)$ {
+		access_log off;
+		expires max;
+	}
+
+	location ~ [^/]\.php(/|$) {
+		fastcgi_split_path_info ^(.+?\.php)(/.*)$;
+		if (!-f \$document_root\$fastcgi_script_name) {
+			return 404;
+		}
+
+		# Mitigate https://httpoxy.org/ vulnerabilities
+		fastcgi_param HTTP_PROXY "";
+
+		fastcgi_pass $NGINX_FCGI_HOST:9000;
+		fastcgi_index index.php;
+
+		# include the fastcgi_param setting
+		include fastcgi_params;
+		fastcgi_intercept_errors on;
+
+		# SCRIPT_FILENAME parameter is used for PHP FPM determining
+		#  the script name. If it is not set in fastcgi_params file,
+		# i.e. /etc/nginx/fastcgi_params or in the parent contexts,
+		fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+	}
+
+	location ~ /\.ht {
+		deny all;
+	}
+}
+EOF
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-WORDPRESS_PATH_OPTION=--path=/var/www
+PHP_FPM_DAEMON_USER=nobody
+PHP_FPM_DAEMON_GROUP=nobody
+WORDPRESS_PATH=/var/www
+WORDPRESS_PATH_OPTION=--path=$WORDPRESS_PATH
 
 wp core is-installed $WORDPRESS_PATH_OPTION
 if [ $? -eq 0 ]
@@ -12,8 +15,20 @@ else
 	# Install WordPress
 	wp core download --locale=ko_KR $WORDPRESS_PATH_OPTION
 	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --locale=ko_KR $WORDPRESS_PATH_OPTION
-	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL $WORDPRESS_PATH_OPTION
+	wp core install --url=https://$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL $WORDPRESS_PATH_OPTION
 	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --user_pass=$WORDPRESS_PASSWORD $WORDPRESS_PATH_OPTION
+	wp theme delete --all $WORDPRESS_PATH_OPTION
+	wp plugin delete $(wp plugin list --status=inactive --field=name $WORDPRESS_PATH_OPTION) $WORDPRESS_PATH_OPTION
 fi
+
+# Enable FTP
+wp config delete FS_METHOD $WORDPRESS_PATH_OPTION
+wp config set FTP_USER wordpress $WORDPRESS_PATH_OPTION
+wp config set FTP_PASS wordpress $WORDPRESS_PATH_OPTION
+wp config set FTP_HOST host_ftp $WORDPRESS_PATH_OPTION
+
+# Disable FTP (temporary)
+wp config set FS_METHOD direct $WORDPRESS_PATH_OPTION
+chown -R $PHP_FPM_DAEMON_USER:$PHP_FPM_DAEMON_GROUP $WORDPRESS_PATH
 
 exec php-fpm8 --nodaemonize
